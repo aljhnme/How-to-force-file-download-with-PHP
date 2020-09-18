7875f7616d8e4460a4ce5f84324aef73a7d1009f
Al-jhnme <52810715+aljhnme@users.noreply.github.com>
2020-09-18 14:01:07 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>How to force file download with PHP</title>
+</head>
+<body>
+
+	<center>
+		<h2 style="color:red;">Welcome To Thapa</h2>
+		<p><b>Click below to dwonload PDF</b></p>
+		<a href="dwonload.php?file=name_pdf">Dwonload file</a>
+	</center>
+
+</body>
+</html>
